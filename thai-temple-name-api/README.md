cf98a7a4cb3196a3d4a9da3aeccd67552887111b
Krissada Sarawit <topfeekrissada@outlook.com>
2023-02-19 10:35:15 +0700

    feat : downloadable csv export

--- thai-temple-name-api/README.md
+++ thai-temple-name-api/README.md
@@ -1,5 +1,9 @@
 visit /temples to get data
 Example: /temples?province=kalasin&province=kamphaengphet		
 
+visit /download to download csv file
+Example: /download?province=kalasin&province=kamphaengphet
+to download all data, just leave the query empty
+
 Available provinces:
 kalasin, kamphaengphet, khonkaen, jantaburi
--- thai-temple-name-api/server.go
+++ thai-temple-name-api/server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +18,10 @@ func main() {
 		ctx.String(200, `
 visit /temples to get data
 Example: /temples?province=kalasin&province=kamphaengphet		
+
+visit /download to download csv file
+Example: /download?province=kalasin&province=kamphaengphet
+to download all data, just leave the query empty
 
 Available provinces:
 kalasin, kamphaengphet, khonkaen, jantaburi
@@ -64,6 +70,59 @@ kalasin, kamphaengphet, khonkaen, jantaburi
 		ctx.String(http.StatusOK, string(payload))
 
 	})
+
+	// Download csv file
+	r.GET("/download", func(ctx *gin.Context) {
+		// TODO : join the query provinces csv file into one file
+		// response the file to download, and delete the file
+		// after download
+
+		// Get query params
+		queryProvinces := ctx.QueryArray("province")
+
+		fmt.Println("Requesting for download : ", queryProvinces)
+
+		// Join csv files
+		filename := strings.Join(queryProvinces, "-") + "-export" + ".csv"
+		if len(queryProvinces) == 0 {
+			filename = "all-temples-export.csv"
+		}
+		output, err := os.Create("../" + filename)
+		if err != nil {
+			fmt.Println(err)
+		}
+		writer := csv.NewWriter(output)
+		for _, province := range queryProvinces {
+			file, err := os.Open(fmt.Sprintf("../%s.csv", province))
+			if err != nil {
+				fmt.Println(err)
+			}
+			reader := csv.NewReader(file)
+			for {
+				record, err := reader.Read()
+				if err != nil {
+					break
+				}
+				writer.Write(record)
+			}
+			file.Close()
+		}
+
+		output.Close()
+		writer.Flush()
+
+		// Response download, with filename
+		ctx.Header("Content-Disposition", "attachment; filename="+filename)
+
+		// Response file
+		if len(queryProvinces) != 0 {
+			ctx.File("../" + filename)
+		} else {
+			// If the query is empty, return all data
+			ctx.File("../temples.csv")
+		}
+	})
+
 	fmt.Println("Server is running on http://localhost:8080")
 	r.Run(":8080") // listen and serve on
 }
